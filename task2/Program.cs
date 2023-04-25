a8ec388c61246e42f9907278ad354477426833b0
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2023-04-25 03:12:37 +0300

    added task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,28 +3,34 @@
 не равны, необходимо как-то оповестить пользователя. Важно: При выполнении метода единственное исключение, 
 которое пользователь может увидеть - RuntimeException, т.е. ваше. */
 
-int[] numer = { 4, 8, 16, 32, 64, 128 };
-int[] denom = { 2, 0, 4, 4, 0, 8 };
-
-if (numer.Length != denom.Length)
-{
-    Console.WriteLine("Длины массивов не равны!");
+int[] arr1 = { 10, 20, 30 };
+int[] arr2 = { 2, 4, 6 };
+if (arr1.Length != arr2.Length)
+    {
+        Console.WriteLine("Длины массивов не равны!");
     return;
-}
-
-for (int i = 0; i < numer.Length; i++)
+    }
+int[] result = DivideArrays(arr1, arr2);
+Console.WriteLine("Result:");
+foreach (int num in result)
 {
-    try
-    {
-        Console.WriteLine(numer[i] + " / " +
-                        denom[i] + " равно " +
-                        numer[i] / denom[i]);
-    }
-    catch (DivideByZeroException)
-    {
-        Console.WriteLine("Делить на нуль нельзя!");
-    }
+    Console.Write($"{num}, ", "");
 }
     
+int[] DivideArrays(int[] arr1, int[] arr2)
+{
+    int[] result = new int[arr1.Length];
+
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        result[i] = arr1[i] / arr2[i];
+    }
+    return result;
+}
+
+
+
+
+
 
 
